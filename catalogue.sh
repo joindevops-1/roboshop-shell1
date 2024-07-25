164ed307b1fff3664e2c991666c853e2fb985409
joindevops <info@joindevops.com>
2024-07-25 19:36:37 +0530

    functions

--- catalogue.sh
+++ catalogue.sh
@@ -84,7 +84,7 @@ systemctl start catalogue &>> $LOGFILE
 
 VALIDATE $? "Starting catalogue"
 
-cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo
+cp /home/ec2-user/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo
 
 VALIDATE $? "copying mongodb repo"
 
